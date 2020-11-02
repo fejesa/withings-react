c08239ba66c99d90e676005f096b5a01eef01997
andras <fejes.andras@dev-labs.com>
2020-11-02 21:23:46 +0100

    :hammer: Token Dao rename

--- src/main/java/io/myhealth/withings/dao/ApplicationTokenDao.java
+++ src/main/java/io/myhealth/withings/dao/ApplicationTokenDao.java
@@ -1,20 +1,20 @@
 package io.myhealth.withings.dao;
 
-import io.myhealth.withings.model.WithingsToken;
+import io.myhealth.withings.model.ApplicationToken;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Mono;
 
 @Component
-public class WithingsTokenDao implements TokenDao {
+public class ApplicationTokenDao implements TokenDao {
 
     @Value("${myhealth.withings.tokenFile}")
     private String tokenFile;
 
     @Override
     public Mono<String> getAccessToken() {
-        return WithingsToken
+        return ApplicationToken
                 .read(tokenFile)
-                .map(WithingsToken::getAccessToken);
+                .map(ApplicationToken::getAccessToken);
     }
 }
